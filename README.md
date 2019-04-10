a8873682c312888962656fc189083370ed8ef6f3
Dominique Rochefort <dominique.rochefort@hmhco.com>
2019-04-10 19:52:12 -0400

    working with bedrock,but not locally

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # experimenting-with-kafka
 Experimenting with Apache Kafka using kafka-node
+
+## Launch Kafka locally
+
+Assuming you have installed [Docker](https://www.docker.com/) and [docker-compose](https://docs.docker.com/compose/) on your computer.
+ 
+```shell
+docker-compose up
+```
+
+
+
---
+++ src/producer.ts
@@ -0,0 +1,27 @@
+import { KafkaClient as Client, KeyedMessage, HighLevelProducer } from 'kafka-node';
+
+const topic: string = 'test';
+
+const client = new Client({ kafkaHost: 'kafka.br.internal:9092', requestTimeout: 100000 });
+const producer = new HighLevelProducer(client);
+
+producer.on(
+    'ready',
+    (): void => {
+        console.log('Producer is ready');
+        const message: string = 'a message';
+        const keyedMessage = new KeyedMessage('keyed', 'a keyed message');
+
+        console.log(`Sending message to ${topic}`);
+        producer.send(
+            [{ topic, messages: [message, keyedMessage] }],
+            (err: Error, result: any): void => {
+                console.log(err || result);
+            }
+        );
+    }
+);
+
+producer.on('error', (err: Error) => {
+    console.log('error', err);
+});
